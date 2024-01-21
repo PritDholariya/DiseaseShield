f8aa1bde576c338e09efe008ae21fe2d535fd9df
{omchikhaliya} <{omchikhaliya121@gmail.com}>
2024-01-21 21:37:47 +0530

    header edit

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -1,10 +1,7 @@
 // Header.js
 
-import React, { useState } from "react";
 import { Icon } from "@iconify/react";
 import IconText from "./components/IconText";
-import TextWithHover from "./components/TextWithHover";
-import logo from "./assets/logo.jpg";
 
 export default function Header() {
 
@@ -13,18 +10,13 @@ export default function Header() {
       {/* This first div will be the left panel */}
       <div className="h-full w-1/5 bg-black flex flex-col justify-between pb-10">
         <div>
-          {/* This div is for logo */}
           <div className="p-6 flex items-center">
-            <img
-              src={logo}
-              alt="DiseaseShield Logo"
-              className="h-10 w-10"
-            />
+            <span className="material-symbols-outlined mr-2 text-white">cardiology</span>
             <h3 className="text-white font-semibold font-serif p-2">
               DiseaseShield
             </h3>
           </div>
-          <div className="py-5">
+          <div>
             <IconText
               iconName={"material-symbols:home"}
               displayText={"Home"}
@@ -38,8 +30,6 @@ export default function Header() {
               iconName={"icomoon-free:books"}
               displayText={"History   "}
             />
-          </div>
-          <div className="pt-5">
             <IconText
               iconName={"material-symbols:add-box"}
               displayText={"Check disease"}
@@ -48,24 +38,19 @@ export default function Header() {
               iconName={"jam:medical"}
               displayText={"Check from symptoms"}
             />
+            <IconText
+              iconName={"mdi:register"}
+              displayText={"Sign Up"}
+            />
+            <IconText
+              iconName={"material-symbols:login-sharp"}
+              displayText={"Log In"}
+            />
           </div>
+
         </div>
-        <div className="text-white font-extrabold w-full">
-                <div className="py-3">
-                    <IconText
-                        iconName={"mdi:register"}
-                        displayText={"Sign Up"}
-                    />
-                </div>
-                <div>
-                <IconText
-                    iconName={"material-symbols:login-sharp"}
-                    displayText={"Log In"}
-                />                
-                </div>
-            </div>
         <div className="px-5">
-            
+
           <div className="border border-gray-100 text-white w-2/5 flex px-2 py-1 rounded-full items-center justify-center hover:border-white cursor-pointer">
             <Icon icon="carbon:earth-europe-africa" />
             <div className="ml-2 text-sm font-semibold">
@@ -73,7 +58,7 @@ export default function Header() {
             </div>
           </div>
         </div>
-        
+
       </div>
     </div>
   );
