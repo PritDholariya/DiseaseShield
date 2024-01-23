a7de51f8f1df20985f3f819c59cb3c5355d863f8
PritDholariya <pritdholariya@gmail.com>
2024-01-23 21:30:30 +0530

    try

--- client/src/components/LoggedInHeader.jsx
+++ client/src/components/LoggedInHeader.jsx
@@ -1,5 +1,5 @@
 
-import React, { useState, useEffect } from "react";
+import React, { useState ,useEffect} from "react";
 import { Icon } from "@iconify/react";
 import IconText from "./IconText";
 import { UserContext } from "../UserContext";
@@ -8,7 +8,7 @@ import axios from "axios"; // Import axios for making HTTP requests
 import { Link } from "react-router-dom";
 
 
-export default function LoggedInHeader({ children, curActiveScreen }) {
+export default function LoggedInHeader({children,curActiveScreen}) {
 
 
   const [isloading, setisloading] = useState(true);
@@ -39,83 +39,84 @@ export default function LoggedInHeader({ children, curActiveScreen }) {
   //       console.error("Error fetching user data:", error);
   //     }
   //   };
-
+  
   //   fetchUserData();
   // }, []);
-
+  
 
   return (
     <>
+    
+    {isloading && <p>loading..</p>}
 
-      {isloading && <p>loading..</p>}
-
-      {!isloading && <div className="h-full w-full flex ">
-        {/* This first div will be the left panel */}
-        <div className="h-full w-1/5 bg-black flex flex-col justify-between pb-10">
-          <div>
-            {/* This div is for logo */}
+    {!isloading && <div className="h-screen w-full flex ">
+      {/* This first div will be the left panel */}
+      <div className="h-full w-1/5 bg-black flex flex-col justify-between pb-10">
+        <div>
+          {/* This div is for logo */}
             <Link to={'/'}>
-              <div className="p-6 flex items-center">
-                <span className="material-symbols-outlined mr-2 text-white">cardiology</span>
-                <h3 className="text-white font-semibold font-serif p-2">
-                  DiseaseShield
-                </h3>
-              </div>
+          <div className="p-6 flex items-center">
+            <span className="material-symbols-outlined mr-2 text-white">cardiology</span>
+            <h3 className="text-white font-semibold font-serif p-2">
+              DiseaseShield
+            </h3>
+          </div>
             </Link>
-            <div className="flex items-center px-6">
-              <img
-                src={user?.profile_image || "https://example.com/user-profile.jpg"}
-                alt="User Profile"
-                className="h-8 w-8 rounded-full"
-              />
-              <div className="ml-2 text-white">{user?.username || "user"}</div>
-            </div>
-            <div className="py-5">
-              <IconText
-                iconName={"material-symbols:home"}
-                displayText={"Home"}
-                targetLink={'/'}
-              />
-              <IconText
-                iconName={"material-symbols:search-rounded"}
-                displayText={"Search"}
-              />
-              <IconText
-                iconName={"icomoon-free:books"}
-                displayText={"History   "}
-                targetLink={'/history'}
-              />
-              <IconText
-                iconName={"material-symbols:add-box"}
-                displayText={"Check disease"}
-                targetLink={"/disease"}
-                active={curActiveScreen === "disease"}
-
-              />
-              <IconText
-                iconName={"jam:medical"}
-                displayText={"Check from symptoms"}
-              />
-
-            </div>
-          </div>
-          <div className="mt-64">
-            <IconText
-              iconName={"material-symbols:logout"}
-              displayText={"Logout"}
+          <div className="flex items-center px-6">
+            <img
+              src={user?.profile_image || "https://example.com/user-profile.jpg"}
+              alt="User Profile"
+              className="h-8 w-8 rounded-full"
             />
+            <div className="ml-2 text-white">{user?.username || "user"}</div>
           </div>
-          <div className="px-5">
-            <div className="border border-gray-100 text-white w-2/5 flex px-2 py-1 rounded-full items-center justify-center hover:border-white cursor-pointer">
-              <Icon icon="carbon:earth-europe-africa" />
-              <div className="ml-2 text-sm font-semibold">
-                English
-              </div>
+          <div className="py-5">
+            <IconText
+              iconName={"material-symbols:home"}
+              displayText={"Home"}
+              targetLink={'/'}
+            />
+            <IconText
+              iconName={"material-symbols:search-rounded"}
+              displayText={"Search"}
+            />
+            <IconText
+              iconName={"icomoon-free:books"}
+              displayText={"History   "}
+              targetLink={'/history'}
+            />
+            <IconText
+              iconName={"material-symbols:add-box"}
+              displayText={"Check disease"}
+              targetLink={"/disease"}
+              active={curActiveScreen === "disease"}
+
+              />
+            <IconText
+              iconName={"jam:medical"}
+              displayText={"Check from symptoms"}
+              targetLink={"/symtoms"}
+              active={curActiveScreen === "symtoms"}
+
+              />
+              <IconText
+                      iconName={"material-symbols:logout"}
+                      displayText={"Logout"}
+              />
+            </div>
+        </div>
+        <div className="px-5">
+          <div className="border border-gray-100 text-white w-2/5 flex px-2 py-1 rounded-full items-center justify-center hover:border-white cursor-pointer">
+            <Icon icon="carbon:earth-europe-africa" />
+            <div className="ml-2 text-sm font-semibold">
+              English
             </div>
           </div>
         </div>
-        {children}
-      </div>}
+      </div>
+      {children}
+    </div>}
     </>
+
   );
 }
